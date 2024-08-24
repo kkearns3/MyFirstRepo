cddf391a15285814ffb00f31a72a386505a7419a
Katy Kearns <kkearns3@ncsu.edu>
2024-08-24 16:39:15 -0400

    Add my first R script

---
+++ FirstScript.R
@@ -0,0 +1,10 @@
+iris
+head(iris)
+avg <- (1+2)/2
+irs <- iris
+
+#get a vector of values from iris
+plength <- iris$Petal.Length
+typeof(plength)
+is.vector(plength)
+hist(plength, main="Histogram of Petal Length")
